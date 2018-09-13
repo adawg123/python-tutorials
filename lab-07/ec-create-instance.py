6d3c5c39e58ca10c6fd59d01583126977b835785
adawg123 <42753436+adawg123@users.noreply.github.com>
2018-09-13 13:45:43 -0500

    Update ec-create-instance.py

--- lab-07/ec-create-instance.py
+++ lab-07/ec-create-instance.py
@@ -2,7 +2,7 @@ import boto3
 
 
 def create_instance():
-    ec2_resource = boto3.resource('ec2')
+    ec2_resource = boto3.resource('ec2',region_name="us-east-1")
     instances = ec2_resource.create_instances(ImageId='ami-6871a115',
                 MinCount=1, MaxCount=1,InstanceType='t2.micro',
                 SecurityGroupIds=['launch-wizard-3'],KeyName='fullstack')
